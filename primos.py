60a9e4bcd4615da6e4b1b95d7cf38c83b021bbff
Amanda Pagani Lima <2200564@SALAS.AULAS>
2023-04-17 20:00:49 -0300

    Adiciona programa primos.py

---
+++ primos.py
@@ -0,0 +1,27 @@
+# Amanda Pagani Lima - RA:2200564
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def encontra_cem_primos():
+    num = 3
+    lista_primos = [2]
+    is_primo = True
+
+    while len(lista_primos) != 100:
+        for divisor in range(2, num):
+            if num % divisor == 0:
+                is_primo = False
+                break
+        if is_primo:
+            lista_primos.append(num)
+        num += 1
+        is_primo = True
+    
+    result = {"lista_primos": lista_primos, "qtd_primos": len(lista_primos)}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
